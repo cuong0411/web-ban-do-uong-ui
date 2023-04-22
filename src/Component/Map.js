f45dfc29cb08fa1d1e516712257aeb7c33ffeb82
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-04-22 11:27:42 +0700

    change map to truong

--- src/Component/Map.js
+++ src/Component/Map.js
@@ -2,10 +2,11 @@ export default function Map() {
   return (
     <section className="map-section">
       <iframe
-        title="Map"
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10499.966498430253!2d2.2944813!3d48.8583701!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8ddca9ee380ef7e0!2zz4DPjc-BzrPOv8-CIM-Ezr_PhSDOkc6Qz4bOtc67!5e0!3m2!1sel!2sgr!4v1609878476869!5m2!1sel!2sgr"
+        title="map"
         id="map"
-      />
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15675.499791276427!2d106.7715774!3d10.8208808!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317526f88d44537d%3A0x582a5580cd585a9f!2zVHLGsOG7nW5nIENhbyDEkOG6s25nIEvhu7kgTmdo4buHIElJ!5e0!3m2!1svi!2s!4v1682137520453!5m2!1svi!2s"
+        loading="lazy"
+      ></iframe>
     </section>
   );
 }
